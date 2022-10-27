8c2ec72987754aff842fcc30e8791419c3c92584
Kristoffel Pirard <kristoffel.pirard@midiagnostics.com>
2022-10-27 09:08:36 +0200

    Add basic interaction

--- src/gitflowbot/cli.py
+++ src/gitflowbot/cli.py
@@ -1,4 +1,5 @@
 import asyncio
+import functools
 import os
 import shutil
 import subprocess
@@ -6,6 +7,7 @@ import tempfile
 from asyncio import gather
 from pathlib import Path
 from textwrap import indent
+from typing import Callable, Awaitable
 
 import click as click
 
@@ -61,22 +63,33 @@ async def task_for(
     repo: Path,
     source_branch: str,
     destination_branch: str,
+    update_view: Callable[[], Awaitable[None]],
 ):
-    assert " " not in feature_name, "that would be a problem for git branch names"
-    branch = f"feature/{feature_name}"
-    dev_repo = play_dir / f"{dev.name}--{feature_name}"
-    git_clone(dev, dev_repo, repo)
-    check_call(dev_repo, git, "checkout", "-b", branch, source_branch)
+    branch, dev_repo = await clone_and_branch_async(
+        dev, feature_name, play_dir, repo, source_branch
+    )
     for c in range(commits):
         await some_work_async(dev_repo, feature_name)
         git_add_file(
             cwd=dev_repo, name=f"{feature_name}.{c}.txt", content=f"{dev.name} was here"
         )
-        git_commit(
-            cwd=dev_repo, message=f"{feature_name} {c}/{commits}"
-        )
+        git_commit(cwd=dev_repo, message=f"{feature_name} {c}/{commits}")
+        git_push(dev_repo)
+        await update_view()
     await git_merge(dev_repo, branch, destination_branch, f"add {feature_name}")
     git_push(dev_repo)
+    check_call(dev_repo, git, "push", "origin", f":{branch}")
+    await update_view()
+
+
+async def clone_and_branch_async(dev, feature_name, play_dir, repo, source_branch):
+    assert " " not in feature_name, "that would be a problem for git branch names"
+    branch = f"feature/{feature_name}"
+    dev_repo = play_dir / f"{dev.name}--{feature_name}"
+    git_clone(dev, dev_repo, repo)
+    check_call(dev_repo, git, "checkout", "-b", branch, source_branch)
+    check_call(dev_repo, git, "push", "--set-upstream", "origin", branch)
+    return branch, dev_repo
 
 
 def git_clone(dev, dev_dir, repo):
@@ -91,7 +104,31 @@ def git_add_file(cwd: Path, name: str, content: str):
     check_call(cwd, git, "add", name)
 
 
-async def git_init(repo: Path):
+async def default_interact(remote: "Remote"):
+    print(f"pausing; update {remote.clone}")
+    check_call(remote.clone, git, "fetch")
+    await asyncio.sleep(0)
+    input(">>>>>>>> press enter to continue")
+
+
+async def default_no_interact(remote: "Remote", delay=1.0):
+    print(f"pausing; update {remote.clone}")
+    check_call(remote.clone, git, "fetch")
+    await asyncio.sleep(delay)
+
+
+class Remote:
+    def __init__(self, bare, clone, interact):
+        self.bare = bare
+        self.clone = clone
+        self.interact = interact
+
+    async def update_view(self):
+        check_call(self.clone, git, "fetch")
+        await self.interact(self)
+
+
+async def git_init(repo: Path, interact):
     assert not repo.exists(), "Can't work with an existing dir"
     check_call(".", git, "init", "--bare", repo)
 
@@ -100,33 +137,34 @@ async def git_init(repo: Path):
     git_add_file(clone, str(clone / "README.md"), "just some text")
     git_commit(clone, "a first commit")
     git_push(clone)
+    return Remote(repo, clone, interact)
 
 
-async def play(play_dir: Path, repo_name: str):
+async def play(play_dir: Path, repo_name: str, interact):
 
     bob, alice, crusty = tuple(map(Dev, ("Bob", "Alice", "Crusty")))
 
     repo = play_dir / repo_name
-    await git_init(repo)
+    remote = await git_init(repo, interact)
 
+    task_on_master = functools.partial(
+        task_for,
+        play_dir,
+        repo=repo,
+        source_branch="master",
+        destination_branch="master",
+        update_view=remote.update_view,
+    )
     tasks = (
-        task_for(
-            play_dir,
-            bob,
-            "feature-1",
+        task_on_master(
+            dev=bob,
+            feature_name="feature-1",
             commits=2,
-            repo=repo,
-            source_branch="master",
-            destination_branch="master",
         ),
-        task_for(
-            play_dir,
-            alice,
-            "feature-2",
+        task_on_master(
+            dev=alice,
+            feature_name="feature-2",
             commits=3,
-            repo=repo,
-            source_branch="master",
-            destination_branch="master",
         ),
     )
 
@@ -139,7 +177,7 @@ def run(play_dir=None, repo=None):
         play_dir = Path(tempfile.TemporaryDirectory().name)
     if repo is None:
         repo = "repo"
-    asyncio.run(play(play_dir, repo))
+    asyncio.run(play(play_dir, repo, functools.partial(default_no_interact, delay=0.1)))
 
 
 if __name__ == "__main__":
